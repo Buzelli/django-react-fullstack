452095d55089199320f20bc145fabad959fa3185
Andre buzelli <buzelli4@gmail.com>
2024-02-05 21:43:54 -0300

    Adicionando redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import AppBar from './components/AppBar';
 import Home from './screens/Home'
+import { Provider } from 'react-redux';
+import store from './store';
+
 function App() {
   return (
-    <div className="App">
-      <AppBar />
-      <Home />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppBar />
+        <Home />
+      </div>
+    </Provider>
+    
   );
 }
 
--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -2,7 +2,16 @@ import React, {useState, useEffect} from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import {produtos} from "../produtos";
+import { UseDispatch, useDispatch, useSelector } from "react-redux";
+import {
+addProduct,
+removeProduct
+} from '../slices/productSlices'
 export default function FormHome() {
+    
+    const dispatch = useDispatch();
+
+    const products = useSelector((state) => state.productStore)
 
     const [item, setItem] = useState('');
     const [quantidade, setQuantidade] = useState('');
@@ -10,7 +19,10 @@ export default function FormHome() {
     const buscarItem = (e) =>{
         const item = produtos.filter((item)=>item.nome.includes(e.toString()))
         console.log(item)
-        setItem(e)
+        if(item){
+          dispatch(addProduct(item));
+        }
+        console.log(products)
     }
 
   return (
@@ -25,8 +37,7 @@ export default function FormHome() {
         <TextField 
             id="outlined-basic" 
             label="Item" 
-            variant="outlined"
-            value={item} 
+            variant="outlined" 
             onChange={(e) => buscarItem(e.target.value)}
         />
         <TextField 
